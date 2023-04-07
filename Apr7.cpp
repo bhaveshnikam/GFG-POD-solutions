8904446bcfd373162c5f96f94d2bcdd28312bcf6
Bhavesh Nikam <96333025+bhaveshnikam@users.noreply.github.com>
2023-04-07 06:37:19 +0530

    Create Apr7.cpp

---
+++ Apr7.cpp
@@ -0,0 +1,20 @@
+int addMinChar(string str){    
+        int n=str.length();
+        int i=0,j=n-1;
+        int ans=0;
+        while(i<=j)
+        {
+            if(str[i]==str[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                ans++; // adding one character at the front
+                i=0;
+                j=n-1-ans;
+            }
+        }
+        return ans;
+    }
